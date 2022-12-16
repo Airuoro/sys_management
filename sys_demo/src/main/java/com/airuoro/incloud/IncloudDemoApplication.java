9e79c23be31babb7394b7e4bc7d291d38f81fe20
Airuo <2510675044@qq.com>
2022-12-16 22:18:26 +0800

    add eureka server

--- sys_demo/src/main/java/com/airuoro/incloud/IncloudDemoApplication.java
+++ sys_demo/src/main/java/com/airuoro/incloud/IncloudDemoApplication.java
@@ -1,11 +1,14 @@
 package com.airuoro.incloud;
 
+import com.airuoro.incloud.demo.controller.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ServletComponentScan
+@ComponentScan(basePackageClasses = UserController.class)
+@EnableDiscoveryClient
 public class IncloudDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(IncloudDemoApplication.class, args);
--- sys_demo/src/main/java/com/airuoro/incloud/demo/controller/UserController.java
+++ sys_demo/src/main/java/com/airuoro/incloud/demo/controller/UserController.java
@@ -1,31 +1,35 @@
 package com.airuoro.incloud.demo.controller;
 
-import com.airuoro.incloud.demo.model.UserModel;
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.EurekaClient;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
 
 @RestController
 @Api(value = "用户接口")
 @RequestMapping("/v1")
 public class UserController {
 
+    @Autowired
+    private EurekaClient client;
+
     @ApiOperation(value = "查询用户", notes = "查询用户")
     @ApiResponses({
             @ApiResponse(code = 1, message = "查询失败")
     })
     @RequestMapping(value = "/users", method = RequestMethod.GET)
-    public UserModel getUser() {
-        UserModel userModel = new UserModel();
-        userModel.setId(1);
-        userModel.setAccount("incloud");
-        userModel.setName("ggy");
-        userModel.setPassword("123123");
-        return userModel;
+    public Set getUser() {
+        Set<String> allKnownRegions = client.getAllKnownRegions();
+        return allKnownRegions;
     }
 
 
